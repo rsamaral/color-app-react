8ea63484ac0e51955371fe4f5fef9dc3ea111212
Raquel Amaral <raquelamaral@Mac.attlocal.net>
2024-11-21 11:54:53 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Palette from './Palette';
+import seedColors from './seedColors';
+
+function App() {
+  return <Palette />;
+}
+
+export default App;
---
+++ src/Palette.js
@@ -0,0 +1,5 @@
+const Palette = () => {
+  return <h1>Palette</h1>;
+};
+
+export default Palette;
